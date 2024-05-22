b84308d36146515f59cd95213288c84a64ebb81d
rondastar <116147793+rondastar@users.noreply.github.com>
2024-05-21 18:29:40 -0700

    added frontend and installed react using vite

---
+++ frontend/components/RecipeInput.jsx
@@ -0,0 +1,14 @@
+function RecipeInput() {
+  return (
+    <div className="component-body">
+      <textarea
+        name="recipe-input"
+        rows="20"
+        cols="50"
+        placeholder="Enter ingredient list here"
+      ></textarea>
+    </div>
+  );
+}
+
+export default RecipeInput;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import RecipeInput from "../components/RecipeInput";
+
+function App() {
+  return (
+    <>
+      <div recipe-converter-container>
+        <RecipeInput />
+      </div>
+    </>
+  );
+}
+
+export default App;
