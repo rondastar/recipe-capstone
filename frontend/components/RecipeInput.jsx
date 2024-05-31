b0b13ef13c5688928e9785c73189725faf98f3a4
rondastar <116147793+rondastar@users.noreply.github.com>
2024-05-31 13:10:47 -0700

    recipe input textarea line splitter functional

--- frontend/components/RecipeInput.jsx
+++ frontend/components/RecipeInput.jsx
@@ -1,12 +1,44 @@
+import { useState, useRef } from "react";
+
 function RecipeInput() {
+  const ref = useRef(null);
+  const [ingredientLines, setIngredientLines] = useState([]);
+  const handleClick = (e) => {
+    // access textarea value
+    const recipeInput = ref.current.value;
+
+    // split recipe input textarea by lines
+    const recipeInputLines = recipeInput.split("\n");
+
+    // store the split recipe input lines in ingredientLines
+    setIngredientLines(recipeInputLines);
+    console.log(ingredientLines);
+  };
   return (
+    // <div className="component-body">
+    //   <label htmlFor={recipeInput}>Original ingredient list:</label>
+    //   <textarea
+    //     id="recipe-input"
+    //     name="recipe-input"
+    //     value={recipeInput}
+    //     onChange={(e) => setRecipeInput(e.target.value)}
+    //     rows="20"
+    //     cols="50"
+    //     placeholder="Enter ingredient list here."
+    //   ></textarea>
+    //   {/* <button onClick={handleClick}>Convert Recipe</button> */}
+    // </div>
     <div className="component-body">
+      <label htmlFor="recipeInput">Original ingredient list:</label>
       <textarea
+        id="recipe-input"
         name="recipe-input"
+        ref={ref}
         rows="20"
         cols="50"
-        placeholder="Enter ingredient list here"
+        placeholder="Enter ingredient list here."
       ></textarea>
+      <button onClick={handleClick}>Convert Units</button>
     </div>
   );
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import RecipeInput from "../components/RecipeInput";
 
